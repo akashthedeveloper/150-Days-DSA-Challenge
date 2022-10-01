108358a0451e67d6c576c4d387e916b9cbf71c8a
akashthedeveloper <0123akashjha@gmail.com>
2022-10-01 13:50:55 +0530

    Added aritmetic operators

--- Python/Day1- DSA/main.py
+++ Python/Day1- DSA/main.py
@@ -9,3 +9,16 @@ print(age)
 
 email = "jk@gmail.com"
 print(email)
+
+#type operator for knowing data-types
+print(type(age))
+print(type(email))
+
+#Arithmetic Operations
+2 + 3  #addition
+3 - 2  #subtraction
+2 * 3  #multiplication 
+2 ** 3 #exponent or power
+3 / 2  #division
+3 // 2  #floor division or integer division which only returns integers
+3 % 2 #modulus it is remainder when deivided by some number
