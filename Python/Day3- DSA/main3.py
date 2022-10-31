e941b9f9bd5370992d9f161e2a4778d5c45f430e
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2022-10-31 21:05:17 +0530

    variables & values

--- Python/Day3- DSA/main3.py
+++ Python/Day3- DSA/main3.py
@@ -4,3 +4,14 @@ print("    /| ")
 print("   / |")
 print("  /  |")
 print(" /___|")
+
+# defining Variables and its values
+
+character_name = "Sam"
+character_age = "35"
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + "yrs old.")
+
+character_name = "Tom"
+print("He really liked the name " + character_name)
+print("but didn't like being " + character_age)
