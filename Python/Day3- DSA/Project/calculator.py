2963acc4e0646c535e3fff4f8f340b39c84a84c8
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2022-10-31 23:44:10 +0530

    game

--- Python/Day3- DSA/Project/calculator.py
+++ Python/Day3- DSA/Project/calculator.py
@@ -1,4 +1,4 @@
-num1 = input("Enter the first number:")
+num1 = input("Enter a number:")
 num2 = input("Enter the another number")
 
 # Here float() function is used to convert string value of num1 & num2 into integer or decimal value
