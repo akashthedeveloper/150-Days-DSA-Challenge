7c4881deb125ffd903bd8be02a06241733b8fc4f
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2022-10-31 23:40:55 +0530

    conversion

---
+++ Python/Day3- DSA/Calculator Project/calc.py
@@ -0,0 +1,6 @@
+num1 = input("Enter the first number:")
+num2 = input("Enter the another number")
+
+# Here float() function is used to convert string value of num1 & num2 into integer or decimal value
+result = float(num1) + float(num2) 
+print(result)
