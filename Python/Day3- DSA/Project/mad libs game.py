37360e785db548820a98b73a41e0063d3e5cf1e0
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2022-11-01 00:08:35 +0530

    fill sentence

---
+++ Python/Day3- DSA/Project/mad libs game.py
@@ -0,0 +1,7 @@
+color = input("Enter a color:")
+plural_noun = input("Enter a Plural Noun:")
+celebrity = input("Enter a celebrity name:")
+
+print("Roses are " + color)
+print(plural_noun + " are purple")
+print("I love " + celebrity) 
