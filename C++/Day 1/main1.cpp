e9458e1d6377f1794454283f3ad0532825b9803c
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2023-03-08 20:26:34 +0530

    changes

--- C++/Day 1/main1.cpp
+++ C++/Day 1/main1.cpp
@@ -8,3 +8,14 @@
            return 0;
        }
 ```
+
+
+# C++ Example
+
+```cpp
+int main() {
+  int y = SOME_MACRO_REFERENCE;
+  int x = 5 + 6;
+  cout << "Hello World! " << x << std::endl();
+}
+```
