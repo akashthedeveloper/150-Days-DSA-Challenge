44f49eb4d523a42042d31229cb4540b34b3fa288
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2023-03-08 20:31:50 +0530

    md

--- C++/Day 1/main1.cpp
+++ C++/Day 1/main1.cpp
@@ -10,12 +10,3 @@
 ```
 
 
-# C++ Example
-
-```cpp
-int main() {
-  int y = SOME_MACRO_REFERENCE;
-  int x = 5 + 6;
-  cout << "Hello World! " << x << std::endl();
-}
-```
---
+++ C++/Day 1/main1.md
@@ -0,0 +1,9 @@
+# C++ Example
+
+```cpp
+int main() {
+  int y = SOME_MACRO_REFERENCE;
+  int x = 5 + 6;
+  cout << "Hello World! " << x << std::endl();
+}
+```
