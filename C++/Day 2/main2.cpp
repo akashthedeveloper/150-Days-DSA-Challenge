e9c0b16fc690d0a01c96666614163a2acaa868ed
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2023-03-14 01:05:16 +0530

    swap

---
+++ C++/Day 2/main2.cpp
@@ -0,0 +1,19 @@
+// Write a program to add 2 numbers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num1 , num2;
+    cout <<"Enter first number" << endl;
+    cin>> num1;
+
+    cout <<"Enter second number" << endl;
+    cin>> num2;
+
+    int sum = num1 + num2;
+    cout <<"Sum of two numbers is " << sum;
+
+    return 0;
+}
---
+++ C++/Day 2/revisemain2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num1, num2;
+    cout << "Enter the first number" <<endl;
+    cin >> num1;
+
+    cout << "Enter the second number" << endl;
+    cin >> num2;
+
+    int sum = num1 + num2 ;
+    cout << "Sum of two number is " << sum << endl;
+
+    return 0;
+}
