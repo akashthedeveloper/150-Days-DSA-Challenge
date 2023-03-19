70ed7c675264beec7a0be17f9f5a77375afecfa6
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2023-03-20 02:23:57 +0530

    assignment

--- C++/Day 4/main4.cpp
+++ C++/Day 4/main4.cpp
@@ -14,13 +14,45 @@ int main() {
     cout << (num1 >= num2) << endl;
     cout << (num1 <= num2) << endl;
 
-    //Arithmetic Operators
+    //Arithmetic Operators 
     cout << num1 + num2 << endl;
     cout << num1 - num2 << endl;
     cout << num1 * num2 << endl;
     cout << num1 / num2 << endl;
     cout << ++num1 << endl; // Here the ++ operator is used before the operand to increase the value from 1
     cout << --num2 << endl; // Here -- operator is used before the operand to decrease the value from 1
+
+    // Logical Operator
+
+    cout << (num1 > num2 && num1 < num2) << endl;
+    cout << (num1 > num2 || num1 < num2) << endl;
+    cout << !(num1 > num2) << endl;
+    cout << !(num1 < num2) << endl;
+    cout << !(num1 == num2) << endl;
+    cout << !(num1 != num2) << endl;
+    cout << !(num1 > num2 && num1 < num2) << endl;
+    cout << !(num1 > num2 || num1 < num2) << endl;
+
+    // Assignment operators
+
+    num1 += 1;
+    cout << num1 << endl;
+
+    num2 -= 2;
+    cout << num2 << endl;
+    
+    num1 /= 2;
+    cout << num2 << endl;
+
+    num2 %= 2;
+    cout << num2 << endl;
+
+    // Bitwise Operator
+
+    cout << (num1 & num2) << endl;
+    cout << (num1 | num2) << endl;
+    cout << (num1 ^ num2) << endl;
+    cout << (~num1) << endl;
     
     return 0;
 }
---
+++ C++/Day 5/main5.cpp
@@ -0,0 +1,18 @@
+// Increase and Decrease the value by using the assignment operator in C++ for 2 numbers.
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a =12 ,b =10;
+
+    a += 3;
+    cout<< a <<endl;
+
+    b -= 3;
+    cout<< b <<endl;
+
+    return 0;
+}
