f32bb0dadb36b70997abcd43f407803b6a960696
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2023-03-22 14:46:05 +0530

    pratice problems

---
+++ C++/Day 6/main6_3.cpp
@@ -0,0 +1,30 @@
+// Write a program to calculate the sum of the first and the second last digit of a 5 digit number.
+
+#include <iostream>
+
+int main()
+{
+    // Declare variable to store user input
+    int num;
+
+    // Prompt the user to enter a 5-digit number
+    std::cout << "Enter a 5-digit number: ";
+
+    // Read the user input and store it in the variable
+    std::cin >> num;
+
+    // Calculate the first digit of the number
+    int first_digit = num / 10000;
+
+    // Calculate the second-last digit of the number
+    int second_last_digit = (num / 10) % 10;
+
+    // Calculate the sum of the first and second-to-last digits
+    int sum = first_digit + second_last_digit;
+
+    // Print out the sum of the first and second-to-last digits
+    std::cout << "Sum of first and second last digits: " << sum << std::endl;
+
+    // Return 0 to indicate successful completion of program
+    return 0;
+}
