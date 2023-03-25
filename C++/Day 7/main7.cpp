36d8d2608bc1827ce003f2e5a89f33c348ce3d0a
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2023-03-26 02:23:53 +0530

    module

--- C++/Day 7/main7.cpp
+++ C++/Day 7/main7.cpp
@@ -1,7 +1,20 @@
-// Conditionals in C++
+// Write a C++ program to print "add" if input value is odd otherwise print "even"
 
-/*
+#include <iostream>
 
+using namespace std;
 
+int main() {
+   
+   int num;
+   cin>>num;
 
-*/
+   if (num%2== 0){
+    cout<<"even" <<endl;
+   }
+   else{
+    cout<<"odd" <<endl;
+   }
+
+   return 0;
+}
