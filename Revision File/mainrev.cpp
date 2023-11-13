dbadd5aae63968403e079b0a753de979d93e3edd
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2023-11-13 19:52:35 +0530

    prime

--- Revision File/mainrev.cpp
+++ Revision File/mainrev.cpp
@@ -431,11 +431,26 @@ int main(){
 
 #include <iostream>
 using namespace std;
-int main(){
-   
-   int n;
-   cin >> n;
-}
 
+int main(){
+
+  int n;
+  cin>>n;
+
+  int i = 2;
+
+  while(i<n) {
+
+    if(n%i==0){
+      cout <<"Not prime" << i << endl;  
+    }
+    else{
+      cout <<"Prime" << i << endl;
+    }
+    i = i+1;
+  }
+
+  return 0;
+}
 
 
