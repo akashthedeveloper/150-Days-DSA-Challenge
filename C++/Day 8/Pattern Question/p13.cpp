e4fbd7869e68d110de4a79e76c4d79ff208b39d4
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2023-11-22 12:16:42 +0530

    p13

---
+++ C++/Day 8/Pattern Question/p13.cpp
@@ -0,0 +1,33 @@
+/*
+A B C
+D E F
+G H I
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 3;  // number of rows and columns
+    
+    int row = 0;
+    char start = 'A';
+    
+    while (row < n) {
+        int col = 0;
+        char ch = start;
+        
+        while (col < n) {
+            cout << ch << " ";
+            ch++;
+            col++;
+        }
+        
+        cout << endl;
+        
+        row++;
+        start += n;
+    }
+    
+    return 0;
+}
