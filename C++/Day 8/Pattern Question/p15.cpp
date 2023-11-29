cb95e86f0e4d98e2bb832642fd7a14f684779923
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2023-11-29 05:51:28 +0530

    p15

---
+++ C++/Day 8/Pattern Question/p15.cpp
@@ -0,0 +1,23 @@
+/*
+A
+B B
+C C C 
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<< char('A'+i-1) << "";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
