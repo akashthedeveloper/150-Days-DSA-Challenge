9a4cae5826ab40b0599a4bb5fcd358a27e91b919
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2023-12-02 22:44:31 +0530

    p23

--- C++/Day 8/Pattern Question/p23.cpp
+++ C++/Day 8/Pattern Question/p23.cpp
@@ -9,4 +9,27 @@
 using namespace std;
 
 int main() {
-    int n = 4; // number of rows
+    int n;
+    cin>>n;
+
+    int row = 1;
+    while(row<=n){
+
+      // Print space
+
+      int space =1;
+      while(space<=row-1){
+        cout<<"  ";
+        space = space + 1;
+      }
+      // Print value 
+      int value =1;
+      while(value<=n-row+1){
+        cout<<row<<" ";
+        value = value + 1;
+      }
+      cout<<endl;
+      row = row + 1;
+      }
+    return 0;
+    }
