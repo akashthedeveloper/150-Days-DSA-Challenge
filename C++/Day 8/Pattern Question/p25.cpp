d31f4e7cfdcbb7123e1d6213e28b79f7f27bc41f
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2023-12-03 10:36:23 +0530

    p25

--- C++/Day 8/Pattern Question/p25.cpp
+++ C++/Day 8/Pattern Question/p25.cpp
@@ -4,3 +4,63 @@
     3 4
       4
 */
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     int row=1;
+//     while(row <=n){
+//         int space=1;
+//         while(space<=row-1){
+//             cout<<"  ";
+//             space++;
+//         }
+//         int col=1;
+//         while(col<=n){
+//             cout<<col<<" ";
+//             col++;
+//         }
+//         cout<<endl;
+//         row++;
+//     }
+// return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int row = 1;
+    while (row <= n) {
+        int space = 1;
+
+        // Print spaces for the current row
+        while (space <= row - 1) {
+            cout << "  ";
+            space++;
+        }
+        
+        // Initialize a variable 'col' with the value of the current row number. 
+        // This is done to start printing numbers from the current row.
+
+        int col = row;
+
+
+        // Print numbers for the current row
+        while (col <= n) {
+            cout << col << " ";
+            col++;
+        }
+
+        cout << endl;
+        row++;
+    }
+
+    return 0;
+}
