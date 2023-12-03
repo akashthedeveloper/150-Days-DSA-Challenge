c74a2ea4135504759a77c8184e431ecf8fda6742
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2023-12-03 10:17:25 +0530

    p24

--- C++/Day 8/Pattern Question/tempCodeRunnerFile.cpp
+++ C++/Day 8/Pattern Question/tempCodeRunnerFile.cpp
@@ -1,3 +1,26 @@
-if (n == 1) {
-        cout << "*";
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int row = 1;
+    while (row <= n) {
+        int space = n - row;
+        while (space) {
+            cout << "  ";
+            space--;
+        }
+
+        int col = 1;
+        while (col <= row) {
+            cout << row << " ";
+            col++;
+        }
+
+        cout << endl;
+        row++;
     }
+
+    return 0;
+}
