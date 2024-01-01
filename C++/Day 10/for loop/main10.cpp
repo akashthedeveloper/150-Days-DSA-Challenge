95fd40a141b9f9e4648e9dea71aeaf3c3a488a20
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2024-01-01 05:53:19 +0530

    type casting

---
+++ C++/Day 10/for loop/main10.cpp
@@ -0,0 +1,17 @@
+/* Type Casting - Converting one data type to another is called type casting*/
+
+/* Note: In type casting if bigger data type value is type casted then the chances of data loss is higher*/
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 66 ;
+    char c = 'b';
+    // a = c; // ASCII value of b will be stored
+
+    c = a;
+
+    cout << c << endl;
+    return 0;
+
+}
