bd3bea667bf4e29f61a67d32354321cad8c9dad8
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2024-01-02 15:22:57 +0530

    typecaste

--- C++/Day 10/for loop/q4for.cpp
+++ C++/Day 10/for loop/q4for.cpp
@@ -1,4 +1,4 @@
-// Write a C++ program to print even number from 1-10 using for loop
+// Write a program for printing odd number
 
 #include <iostream>
 using namespace std;
@@ -8,8 +8,8 @@ int main(){
     cout<<"Enter the number:" <<endl;
     cin>>n;
 
-    for(int i=2; i<=n; i=i+2){
-        if(i%2==0){
+    for(int i=1; i<=n; i=i+2){
+        if(i%2 != 0){
             cout << i << endl;
         }        
     }
---
+++ C++/Day 10/for loop/q5for.cpp
@@ -0,0 +1,11 @@
+// Write a C++ program to print char from a-z
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    for(char name ='a'; name<='z'; name=name+1){
+        cout<<name<<" ";
+    }
+return 0;
+}
