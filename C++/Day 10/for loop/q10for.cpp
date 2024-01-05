eece4e3435b3a31e82a94b5845ea46003207c169
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2024-01-05 10:26:34 +0530

    n

--- C++/Day 10/for loop/q10for.cpp
+++ C++/Day 10/for loop/q10for.cpp
@@ -1,2 +1,22 @@
 // Sum of n natural number
 
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number : ";
+    cin>>n;
+    // int sum=0;
+    // for(int i=1;i<=n;i++){
+    //     sum+=i; // sum = sum + i
+    // }
+    // cout<<"Sum is : "<<sum;
+    
+    // OR another method
+
+    int sum = (n*(n+1))/2;
+
+    cout<<"Sum is : "<<sum;
+    
+    return 0;
+}  
