fd6754dc00a979609399df82f711bf5b2b628586
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2024-10-03 07:51:31 +0530

    sizeof()

---
+++ Revision File/rev2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int a = 5; //Define integer a
+    int size = sizeof(a); // Use sizeof() operator to find the size of a or data types
+
+    cout << "Size of a is: "<<size <<endl;
+
+    return 0;
+}
