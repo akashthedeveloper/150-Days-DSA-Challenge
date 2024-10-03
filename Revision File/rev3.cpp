603b7cd379459d150f84f5630275150932571390
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2024-10-03 08:21:53 +0530

    rel

---
+++ Revision File/rev3.cpp
@@ -0,0 +1,15 @@
+// Typecasting - Converting one data types to another with the help of  ASCII table is called typecasting
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 'a';
+    cout << a << endl;
+
+    char c = 98;
+    cout << c << endl;
+
+    return 0;
+}
---
+++ Revision File/rev4.cpp
@@ -0,0 +1,30 @@
+// Relational Operator
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a =3;
+    int b =6;
+
+    bool n1 = (a==b);
+    cout << n1 <<endl;
+
+    bool n2 = (a>=b);
+    cout << n2 <<endl;
+
+    bool n3 = (a<=b);
+    cout << n3 <<endl;
+
+    bool n4 = (a>b);
+    cout << n4 <<endl;
+
+    bool n5 = (a<b);
+    cout << n5 <<endl;
+
+    bool n6 = (a!=b);
+    cout << n6 <<endl;
+
+    return 0;
+}
